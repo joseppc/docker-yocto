bf791dea95c1df59609d99a5bc5e810385592b47
Josep Puigdemont <josep.puigdemont@gmail.com>
2018-10-19 09:56:09 +0200

    entrypoint.sh: add group
    Signed-off-by: Josep Puigdemont <josep.puigdemont@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,6 +18,9 @@ fi
 USER_UID=${HOST_UID:=$UID}
 USER_GID=${HOST_GID:=$GID}
 
+# Create group
+groupadd -g $(USER_GID) > /dev/null 2>&1
+
 # Create user
 useradd ${USER} --shell /bin/bash --create-home \
 	--uid ${USER_UID} --gid ${USER_GID} > /dev/null 2>&1
